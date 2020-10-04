281d8851a9079c226b3b4e0c1ba76100b4b4f402
Ravi Trivedi <travi@redhat.com>
2020-10-04 09:50:51 +0530

    Adding files

---
+++ helpers/helpers.go
@@ -0,0 +1,7 @@
+package helpers
+
+import "fmt"
+
+func PrintA() {
+	fmt.Println("Inside PrintA!")
+}
---
+++ sample.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/ravitri/go-repo-a/helpers"
+	"github.com/ravitri/go-repo-b"
+)
+
+func main() {
+	b.PrintB()
+	helpers.PrintA()
+}
